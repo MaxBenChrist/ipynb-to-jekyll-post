cf15e51c5d503ec8a2502400b480bfa225409254
Max Christ <max.christ@me.com>
2016-03-13 13:41:00 +0100

    use python script to remove prompt xml nodes

--- create_post_notebook.sh
+++ create_post_notebook.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 file_nb="$1"
-ipython nbconvert --to html $file_nb
-#ipython nbconvert --template basic $file_nb
+ipython nbconvert --template basic $file_nb
 
 # get date and split it into day and time
 DATE=`date +%Y-%m-%d/%H:%M:%S`
@@ -12,15 +11,15 @@ blogtitle=${file_nb//.ipynb/}
 blogtitle=${blogtitle//_/ }
 
 file_html=${file_nb//.ipynb/.html}
-output=$d"-$file_html"
-
+output=$d"-"${file_html//.html/.md}
+echo $output
 # Add header to file 
 header="---\nlayout: post\ntitle:  \"$blogtitle\"\nsubtitle: \ndate:   $d $t \ncategories: [notebook] \n--- \nThis is the notebook $file_nb"
 
 echo -e $header > $output
+python strip_promt_nodes.py $file_html 
 
 #delete the input prompts and add to file
-#awk '!/input_prompt/' $file_html >>  $output
 cat $file_html >>  $output
 rm $file_html
 
